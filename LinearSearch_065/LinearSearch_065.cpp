978f75d4523d1f9d39ed33b9042ee6f8506a132c
bimbim1214 <bimoadityapangestu@gmail.com>
2023-04-14 15:36:53 +0700

    menambahkan int main untuk memanggil hasil

--- LinearSearch_065/LinearSearch_065.cpp
+++ LinearSearch_065/LinearSearch_065.cpp
@@ -49,3 +49,18 @@ void linearSearch()
 			}
 		}
 
+		if (i == n) //langkah 5B not found
+			cout << "\n" << item << "not found in the array\n";
+		cout << "\nNumber of comparation: " << comparation << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'y'));
+}
+
+
+int main()
+{
+	input();
+	linearSearch();
+}
