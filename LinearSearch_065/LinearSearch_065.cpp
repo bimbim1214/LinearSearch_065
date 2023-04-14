199289cb56cdceb0da5268824b2b7557c2d9a711
bimbim1214 <bimoadityapangestu@gmail.com>
2023-04-14 15:33:29 +0700

    menambahkan perulangan for untuk

--- LinearSearch_065/LinearSearch_065.cpp
+++ LinearSearch_065/LinearSearch_065.cpp
@@ -6,3 +6,23 @@ int n;
 int i;
 
 void input()
+{
+	while (true)
+	{
+		cout << "enter the number of element in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 element. \n\n";
+	}
+	cout << "\n-----------------\n";
+	cout << "enter array element\n";
+	cout << "-------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
+}
+
